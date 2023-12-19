fb9e184a9c7f38b366b2f007d64b58df6bed3f35
yRagax <01168917@pw.edu.pl>
2023-12-19 14:53:36 +0100

    remove param loading from alarm clear launch file

--- dobot_alarm_clear/launch/dobot_alarm_clear.launch.py
+++ dobot_alarm_clear/launch/dobot_alarm_clear.launch.py
@@ -1,16 +1,7 @@
-import os
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import ExecuteProcess
-from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
-
-    config = os.path.join(
-        get_package_share_directory('dobot_alarm_clear'),
-        'config',
-        'alarm_clear_params.yaml'
-        )
 
     return LaunchDescription([
         Node(
@@ -18,11 +9,4 @@ def generate_launch_description():
             executable='alarm_clear',
             output='screen',
         ),
-    ExecuteProcess(
-        cmd=[[
-            'sleep 5;', 'ros2 ' , 'param ',  'load ', '/dobot_alarm_clear_srv ', config
-        ]],
-        shell=True
-    )
     ])
-

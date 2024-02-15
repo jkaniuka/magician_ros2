18cc2b1a1ca7d08105cc34b877d3c3786477a6fa
jkaniuka <kan.jan@wp.pl>
2024-02-15 18:59:43 +0100

    alarms cleanup before startup

--- dobot_bringup/dobot_bringup/set_tool_null.py
+++ dobot_bringup/dobot_bringup/set_tool_null.py
@@ -8,6 +8,8 @@ class SetToolNullConfigurator(Node):
     def __init__(self):
         super().__init__('dobot_init_config')
         print("Loading the initial configuration into the robot.")
+        # Reset old alarms before startup
+        bot.clear_alarms_state()
         bot.set_end_effector_params(0, 0, 0)
 
 def main(args=None):
